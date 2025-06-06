bf69983b7443a2b308083a98aff2c80e9167a9f1
Nada Firda Khofifah <143942532+nadafirdakhofifah@users.noreply.github.com>
2025-06-06 21:02:05 +0700

    AdminController.java

--- controller/AdminController.java
+++ controller/AdminController.java
@@ -1,16 +1,22 @@
 package controller;
 
 import dao.AdminDAO;
+import dao.UserDAO;
 import model.Absensi;
+import model.User;
+
 import java.util.List;
 
 public class AdminController {
     private final AdminDAO adminDAO;
+    private final UserDAO userDAO;
 
     public AdminController() {
         this.adminDAO = new AdminDAO();
+        this.userDAO = new UserDAO();
     }
 
+    // ===== Absensi Related =====
     public List<Absensi> getAllAbsensi() {
         return adminDAO.getAllAbsensi();
     }
@@ -22,4 +28,30 @@ public class AdminController {
     public List<Absensi> searchAbsensi(String keyword) {
         return adminDAO.searchAbsensi(keyword);
     }
+
+    // ===== Karyawan Related =====
+    public List<User> getAllKaryawan(int page, int pageSize) {
+        return userDAO.getAllKaryawan(page, pageSize);
+    }
+
+    public boolean insertKaryawan(String name, String nim, String password) {
+        return userDAO.insert(name, nim, password, "karyawan");
+    }
+
+    public User getKaryawanByNim(String nim) {
+        return userDAO.getByNim(nim);
+    }
+
+    public boolean updateKaryawan(int id, String name, String password) {
+        return userDAO.update(id, name, password);
+    }
+
+    public boolean deleteKaryawanByNim(String nim) {
+        return userDAO.deleteByNim(nim);
+    }
+
+    public List<User> searchKaryawan(String keyword) {
+        return userDAO.search(keyword);
+    }
 }
+
