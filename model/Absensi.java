c8bfda351143bb76d15abef4636c930e5d5c8916
Fajrul Falaq Z. Malangke <143930065+fajrulfzm@users.noreply.github.com>
2025-06-09 15:19:45 +0700

    .

--- model/Absensi.java
+++ model/Absensi.java
@@ -1,29 +1,49 @@
-package com.example.aplikasiabsensi.model;
+package model;
+
+import java.sql.Date;
 
 public class Absensi {
+    // Untuk controller AbsensiController
     private String nim;
     private String jenisAbsen;
 
-    // Constructor
+    // Untuk AdminDAO
+    private int id;
+    private int userId;
+    private Date tanggal;
+    private String status;
+
+    // Constructor untuk AbsensiController
     public Absensi(String nim, String jenisAbsen) {
         this.nim = nim;
         this.jenisAbsen = jenisAbsen;
     }
 
-    // Getter and Setter
-    public String getNim() {
-        return nim;
+    // Constructor untuk AdminDAO
+    public Absensi(int id, int userId, Date tanggal, String status) {
+        this.id = id;
+        this.userId = userId;
+        this.tanggal = tanggal;
+        this.status = status;
     }
 
-    public void setNim(String nim) {
-        this.nim = nim;
-    }
+    // Getter dan Setter untuk nim dan jenisAbsen
+    public String getNim() { return nim; }
+    public void setNim(String nim) { this.nim = nim; }
 
-    public String getJenisAbsen() {
-        return jenisAbsen;
-    }
+    public String getJenisAbsen() { return jenisAbsen; }
+    public void setJenisAbsen(String jenisAbsen) { this.jenisAbsen = jenisAbsen; }
 
-    public void setJenisAbsen(String jenisAbsen) {
-        this.jenisAbsen = jenisAbsen;
-    }
+    // Getter dan Setter untuk id, userId, tanggal, status
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public Date getTanggal() { return tanggal; }
+    public void setTanggal(Date tanggal) { this.tanggal = tanggal; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
