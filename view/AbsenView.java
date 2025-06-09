76ea9e9238d0f0072ff3ebe08da7732eb481b88f
Fajrul Falaq Z. Malangke <143930065+fajrulfzm@users.noreply.github.com>
2025-06-09 12:13:18 +0700

    absenView.java

--- view/AbsenView.java
+++ view/AbsenView.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public abstract class AbsenView {
 
-    // Scanner untuk membaca input pengguna
     protected Scanner scanner;
 
-    // Konstruktor untuk menginisialisasi scanner
+    // Konstruktor
     public AbsenView() {
         this.scanner = new Scanner(System.in);
     }
 
-    // Metode pembantu untuk menampilkan informasi umum atau error
     protected void printMessage(String message) {
         System.out.println(message);
     }
 
-    // Metode pembantu untuk meminta input string
     protected String getInput(String prompt) {
         System.out.print(prompt);
         return scanner.nextLine();
     }
 
-    // Metode pembantu untuk menutup scanner
     public void closeScanner() {
         if (scanner != null) {
             scanner.close();
