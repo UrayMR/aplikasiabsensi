252850c23f5c2635204beb90ca2d3189f474f0a2
Fajrul Falaq Z. Malangke <143930065+fajrulfzm@users.noreply.github.com>
2025-06-09 10:49:14 +0700

    absensi

--- controller/AbsensiController.java
+++ controller/AbsensiController.java
@@ -1,16 +1,41 @@
-package controller;
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 
-import dao.AbsensiDAO;
+const app = express();
+const port = 3000;
 
-public class AbsensiController {
-    private final AbsensiDAO absensiDAO;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    public AbsensiController() {
-        this.absensiDAO = new AbsensiDAO();
+// Membuat koneksi ke database
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  password: '', 
+  database: 'aplikasiabsensi', 
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to database');
+});
+
+// Route untuk input absensi
+app.post('/absen', (req, res) => {
+  const { nim, jenis_absen } = req.body;
+
+  const query = `INSERT INTO absensi (nim, jenis_absen) VALUES (?, ?)`;
+  
+  db.query(query, [nim, jenis_absen], (err, result) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      res.send('Terjadi kesalahan saat absen.');
+    } else {
+      res.send('Absensi berhasil dilakukan!');
     }
+  });
+});
 
-    // Fungsi submitAbsen
-    
-    // Fungsi getUserIdByNIM
-
-}
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
